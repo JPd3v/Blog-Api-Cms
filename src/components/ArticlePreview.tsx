08291d198df531a584704cd220331d17059fff31
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-11-19 04:27:44 -0300

    added components to fetch current user articles

---
+++ src/components/ArticlePreview.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import type { Article } from './UserArticles';
+
+interface ComponentProps {
+  article: Article;
+}
+
+export default function ArticlePreview({ article }: ComponentProps) {
+  const articleDate = new Date(article.published_date).toLocaleDateString(
+    'en-US',
+    { year: 'numeric', month: 'long', day: 'numeric' }
+  );
+
+  return (
+    <>
+      <Link to={`/article/${article._id}`} className="article-preview__title">
+        {article.title}
+      </Link>
+      <p
+        className={`article-preview__state ${
+          article.published
+            ? 'article-preview__state--published'
+            : 'article-preview__state--not-published'
+        }`}
+      >
+        {article.published ? 'Published' : 'Not Published'}
+      </p>
+      <Link to={`/article/${article._id}`} className="article-preview__content">
+        {article.content}
+      </Link>
+
+      <p className="article-preview__date">{articleDate}</p>
+    </>
+  );
+}
--- src/components/UserArticles.tsx
+++ src/components/UserArticles.tsx
@@ -1,9 +1,81 @@
-// import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import useAuth from '../hooks/useAuth';
+import LoadingPage from '../utils/LoadingPage';
+import ArticlePreview from './ArticlePreview';
+
+interface Article {
+  author: Author;
+  title: string;
+  content: string;
+  published: boolean;
+  published_date: string;
+  _id: string;
+}
+
+export type { Article };
+
+interface Author {
+  first_name: string;
+  last_name: string;
+  _id: string;
+}
 
 export default function UserArticles() {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [fetchError, setFetchError] = useState('');
+  const { userToken } = useAuth();
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function getArticles() {
+      try {
+        const req = await fetch(
+          'https://blog-api-787a.onrender.com/user/articles',
+          {
+            method: 'GET',
+            credentials: 'include',
+            headers: { Authorization: `Bearer ${userToken}` },
+            signal: controller.signal,
+          }
+        );
+
+        const response = await req.json();
+        setArticles(response);
+        setIsLoading(false);
+      } catch (error) {
+        if (!controller.signal.aborted) {
+          setIsLoading(false);
+          console.log(error);
+          setFetchError('Error geting articles form the server');
+        }
+      }
+    }
+    getArticles();
+
+    return () => controller.abort();
+  }, []);
+
   return (
-    <div>
-      <div> map render user post</div>
+    <div className="articles-container">
+      {isLoading && !fetchError ? (
+        <LoadingPage />
+      ) : (
+        articles.map((article) => (
+          <div
+            key={article._id}
+            className="articles-container__article-preview article-preview"
+          >
+            <ArticlePreview article={article} />
+          </div>
+        ))
+      )}
+      {!isLoading && fetchError ? <p>{fetchError}</p> : null}
+
+      {!isLoading && !fetchError && articles.length < 1 ? (
+        <p>You don&apos;t have articles created</p>
+      ) : null}
     </div>
   );
 }
