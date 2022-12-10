8d2270c6859a58564d57dc2783a7823591612593
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-10 14:34:58 -0300

    fixed bug in the menu which did not close itself..
     when clicking on a link and also added a limit of characters
     on the preview cards of articles

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 
 interface Props {
@@ -5,18 +6,44 @@ interface Props {
   handleClick: () => void;
 }
 
+// interface Iasd{
+//   ref: React.MutableRefObject<null>;
+//   closeMenu: () => void;
+// }
+
+function closeMenu(ref: React.MutableRefObject<any>, callBack: () => void) {
+  useEffect(() => {
+    function handleCloseMenu(event: MouseEvent) {
+      if (
+        ref.current &&
+        ref.current.contains(event.target) &&
+        ref.current !== event.target
+      ) {
+        callBack();
+      }
+    }
+    document.addEventListener('click', (event) => handleCloseMenu(event));
+    return () => {
+      document.removeEventListener('click', (event) => handleCloseMenu(event));
+    };
+  }, [ref]);
+}
+
 export default function HamburgerMenu({ children, handleClick }: Props) {
+  const hamburgerMenuRef = useRef(null);
+
+  closeMenu(hamburgerMenuRef, handleClick);
+
   return (
     <div className="hamburger-menu">
-      <div className="hamburger-menu__content">
+      <div className="hamburger-menu__content" ref={hamburgerMenuRef}>
         {children}
         <button
           type="button"
           className="hamburger-menu__close-button"
-          onClick={() => handleClick()}
           aria-label="close navigation menu"
         >
-          <AiOutlineClose />{' '}
+          <AiOutlineClose />
         </button>
       </div>
     </div>
